181764a928cb0615a1cec3106a9a32f9f7a9c975
V3DK <ved.kenjale@icloud.com>
2021-11-21 00:00:17 -0500

    first chaotic orbits found, searching for stable lemniscate

--- projectTest.py
+++ projectTest.py
@@ -1,6 +1,7 @@
 from math import *
 from vpython import *
-#0.4,0.2,0.6
+from heapq import nsmallest
+
 brown = vector(0.4,0.2,0.6)
 
 minRad = 1e6
@@ -15,28 +16,28 @@ earthMass = 5.972e24
 sunMass = 1.989e30
 moonMass = 7.347673e22
 
-ved = sphere(pos=vector(sunEarthDist,0,0),radius=minRad,color=color.rgb_to_hsv(brown))
-tilden = sphere(pos=vector(-d,0,0),radius=minRad,color=color.white)
-chamy = sphere(pos=vector(0,0,0),radius=minRad * 1, color=color.yellow)
+ved = sphere(pos=vector(d * (320 / 360),-d * (80 / 360), 0),radius=minRad,color=color.rgb_to_hsv(brown))
+tilden = sphere(pos=vector(0,0,0),radius=minRad,color=color.white)
+chamy = sphere(pos=vector(-d * (320 / 360),d * (80 / 360), 0),radius=minRad * 1, color=color.yellow)
 
 G = 6.67e-11
 
-ved.mass = earthMass
+ved.mass = 1e23
 tilden.mass = 1e23
-chamy.mass = sunMass
+chamy.mass = 1e23
 
-p = 1
-ved.speed = eSpeed * p
-tilden.speed = -p * 100 * 0
-chamy.speed = 0
+p = 1.7
+ved.speed = 100 * p
+tilden.speed = 100 * p
+chamy.speed = 100 * p
 
-ved.velocity = vector(0, ved.speed, 0)
+ved.velocity = vector(ved.speed, ved.speed, 0)
 ved.accel = vector(0, 0, 0)
 
-tilden.velocity = vector(0, tilden.speed, 0)
+tilden.velocity = vector(-tilden.speed, -tilden.speed, 0)
 tilden.accel = vector(0, 0, 0)
 
-chamy.velocity = vector(0, chamy.speed, 0)
+chamy.velocity = vector(chamy.speed, chamy.speed, 0)
 chamy.accel = vector(0, 0, 0)
 
 ved.trail = curve(color=color.red) #,retain=250)
@@ -47,39 +48,63 @@ ved.trail.append(pos = ved.pos)
 tilden.trail.append(pos=tilden.pos)
 chamy.trail.append(pos=chamy.pos)
 
-
-
-rTV = ved.pos - tilden.pos
-rTC = chamy.pos - tilden.pos
-rCV = ved.pos - chamy.pos
-fTV = ((-(G) * (tilden.mass * ved.mass)) * (1 / (mag2(rTV)))) * norm(rTV)
-fTC = ((-(G) * (tilden.mass * chamy.mass)) * (1 / (mag2(rTC)))) * norm(rTC)
-fCV = ((-(G) * (chamy.mass * ved.mass)) * (1 / (mag2(rCV)))) * norm(rCV)
-ved.force = fCV
-# tilden.force = vector(0, 0, 0)
-chamy.force = -fCV
+#setup other variables
+rTV = vector(0, 0, 0)
+rTC = vector(0, 0, 0)
+rCV = vector(0, 0, 0)
+fTV = vector(0, 0, 0)
+fTC = vector(0, 0, 0)
+fCV = vector(0, 0, 0)
+ved.force = vector(0, 0, 0)
+tilden.force = vector(0, 0, 0)
+chamy.force = vector(0, 0, 0)
 
 def update():
+    #update distances
     rTV = ved.pos - tilden.pos
     rTC = chamy.pos - tilden.pos
     rCV = ved.pos - chamy.pos
     fTV = ((-(G) * (tilden.mass * ved.mass)) * (1 / (mag2(rTV)))) * norm(rTV)
     fTC = ((-(G) * (tilden.mass * chamy.mass)) * (1 / (mag2(rTC)))) * norm(rTC)
     fCV = ((-(G) * (chamy.mass * ved.mass)) * (1 / (mag2(rCV)))) * norm(rCV)
-    ved.force = fCV
-    # tilden.force = vector(0, 0, 0)
-    chamy.force = -fCV
+
+    #update forces
+    ved.force = fTV + fCV
+    tilden.force = -(fTV + fTC)
+    chamy.force = fTC - fCV
+
+    #update accels
     ved.accel = ved.force / ved.mass
-    #tilden.accel = tilden.force / tilden.mass
+    tilden.accel = tilden.force / tilden.mass
     chamy.accel = chamy.force / chamy.mass
 
+#update before while loop starts
+update()
+
+
+maxDT = 700
+minDT = 10
+
+TVInitDist = mag(ved.pos - tilden.pos)
+TCInitDist = mag(chamy.pos - tilden.pos)
+CVInitDist = mag(ved.pos - chamy.pos)
+
 time = 0
-dt = 3600
+dt = maxDT
+
 
 run = True
 
 while run:
+    #need these here...not sure why
+    rTV = ved.pos - tilden.pos
+    rTC = chamy.pos - tilden.pos
+    rCV = ved.pos - chamy.pos
+
     rate(500)
+
+    if ((mag(rTV) < minRad) or (mag(rTC) < minRad) or (mag(rCV) < minRad)):
+        run = False
 
     #mark last position
     prev = ved.pos.y
@@ -87,15 +112,18 @@ while run:
     #update pos via verlet velocity
     ved.pos += (ved.velocity * dt) + ((0.5 * ved.accel) * (dt**2))
     chamy.pos += (chamy.velocity * dt) + ((0.5 * chamy.accel) * (dt**2))
+    tilden.pos += (tilden.velocity * dt) + ((0.5 * tilden.accel) * (dt ** 2))
 
-    # update accel and velocity
+    #update accel and velocity
     vedAccelCurr = ved.accel
     chamyAccelCurr = chamy.accel
+    tildenAccelCurr = tilden.accel
 
     update()
 
     ved.velocity += (vedAccelCurr + ved.accel) * (0.5 * dt)
     chamy.velocity += (chamyAccelCurr + chamy.accel) * (0.5 * dt)
+    tilden.velocity += (tildenAccelCurr + tilden.accel) * (0.5 * dt)
 
     #trails
     ved.trail.append(pos = ved.pos)
@@ -112,6 +140,24 @@ while run:
 
     #"""
 
+    #determining dt based on dt
+    avgDist = (mag(rTV) + mag(rTC) + mag(rCV)) * (1/3)
+    TVCurrDist = mag(rTV)
+    TCCurrDist = mag(rTC)
+    CVCurrDist = mag(rCV)
+
+    list = [TVCurrDist, TCCurrDist, CVCurrDist]
+
+    minDist = nsmallest(1, list)
+
+
+    dt = ceil(((minDist[0] / avgDist) * (maxDT - minDT)) + minDT)
+    if(dt > maxDT):
+        dt = maxDT
+
      # update for next iteration
     time += dt
+
+    #print(dt)
+
 
